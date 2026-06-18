309e1ef302e4bc5739ff2c5dd1ba1754e71e4b8e
Kajtek-hub <gkajetan@student.agh.edu.pl>
2026-06-19 00:14:05 +0200

    Clean up root directory

--- android/app/src/main/kotlin/com/example/magisterka/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.magisterka
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:magisterka/app_start.dart';
-
-
-void main() {
-  runApp(
-    AppStart()
-  );
-}
